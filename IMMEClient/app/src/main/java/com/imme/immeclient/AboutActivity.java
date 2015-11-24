de4d2972492007efc79d3dccf07317c4b1adb2e2
Dhuta Pratama <me@dhutapratama.com>
2015-11-25 00:33:08 +0700

    Receive input update

--- IMMEClient/app/src/main/java/com/imme/immeclient/AboutActivity.java
+++ IMMEClient/app/src/main/java/com/imme/immeclient/AboutActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
--- IMMEClient/app/src/main/java/com/imme/immeclient/ReceiveActivity.java
+++ IMMEClient/app/src/main/java/com/imme/immeclient/ReceiveActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.readystatesoftware.systembartint.SystemBarTintManager;
@@ -19,6 +20,10 @@ import org.w3c.dom.Text;
 
 
 public class ReceiveActivity extends AppCompatActivity {
+    String money = "0";
+    Button button_0, button_1, button_2, button_3, button_4, button_5, button_6, button_7, button_8, button_9;
+    ImageButton button_backspace;
+    TextView balance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,6 +73,142 @@ public class ReceiveActivity extends AppCompatActivity {
             }
         });
 
+        button_0 = (Button) findViewById(R.id.button_0);
+        button_1 = (Button) findViewById(R.id.button_1);
+        button_2 = (Button) findViewById(R.id.button_2);
+        button_3 = (Button) findViewById(R.id.button_3);
+        button_4 = (Button) findViewById(R.id.button_4);
+        button_5 = (Button) findViewById(R.id.button_5);
+        button_6 = (Button) findViewById(R.id.button_6);
+        button_7 = (Button) findViewById(R.id.button_7);
+        button_8 = (Button) findViewById(R.id.button_8);
+        button_9 = (Button) findViewById(R.id.button_9);
+        button_backspace = (ImageButton) findViewById(R.id.button_backspace);
+
+        balance = (TextView) findViewById(R.id.receive_balance_value);
+        button_0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "0";
+                if (money.equals("00")) {
+                    money = "0";
+                } else {
+                    balance.setText(money);
+                }
+            }
+        });
+
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "1";
+                if (money.equals("01")) {
+                    money = "1";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "2";
+                if (money.equals("02")) {
+                    money = "2";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "3";
+                if (money.equals("03")) {
+                    money = "3";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "4";
+                if (money.equals("04")) {
+                    money = "4";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "5";
+                if (money.equals("05")) {
+                    money = "5";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "6";
+                if (money.equals("06")) {
+                    money = "6";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "7";
+                if (money.equals("07")) {
+                    money = "7";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "8";
+                if (money.equals("08")) {
+                    money = "8";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "9";
+                if (money.equals("09")) {
+                    money = "9";
+                }
+                balance.setText(money);
+            }
+        });
+
+        button_backspace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                money = money + "0";
+                if (money.length() == 1) {
+                    money = "0";
+                }else {
+                    money = money.substring(0, money.length()-2);
+                }
+                balance.setText(money);
+            }
+        });
     }
     
     @Override
